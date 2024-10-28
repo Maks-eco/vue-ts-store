9732b7e224d468708ba0e0a5cc1c92434532fa0c
Maks-eco <maks.zawulon@gmail.com>
2024-10-29 01:56:29 +0400

    Updated base path

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,20 +1,23 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  compatibilityDate: '2024-04-03',
-  devtools: { enabled: true },
-  srcDir: 'src/',
-  css: ['@/global/styles.less'],
-  modules: ['@pinia/nuxt', '@nuxtjs/i18n'],
-  vite: {
-    css: {
-      preprocessorOptions: {
-        less: {
-          additionalData: '@use "~/global/colors.less" as *;',
-        },
-      },
+    compatibilityDate: '2024-04-03',
+    devtools: { enabled: true },
+    app: {
+        baseURL: '/vue-ts-store/',
     },
-  },
-  i18n: {
-    vueI18n: '@/plugins/i18n.config.ts',
-  },
+    srcDir: 'src/',
+    css: ['@/global/styles.less'],
+    modules: ['@pinia/nuxt', '@nuxtjs/i18n'],
+    vite: {
+        css: {
+            preprocessorOptions: {
+                less: {
+                    additionalData: '@use "~/global/colors.less" as *;',
+                },
+            },
+        },
+    },
+    i18n: {
+        vueI18n: '@/plugins/i18n.config.ts',
+    },
 })
